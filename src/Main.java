9a50b59ede003488ef9e174fac6147e8ca4e71d1
shivapathak-code <shivapathak766@gmail.com>
2026-09-09 00:52:54 +0530

    min to max heap

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,78 @@
 public class Main {
         static class Solution {
+//
+//            public boolean isHeap(int[] nums) {
+//
+//                int n = nums.length;
+//
+//                for (int i = 0; i < n / 2; i++) {
+//
+//                    int left = 2 * i + 1;
+//                    int right = 2 * i + 2;
+//
+//                    if (left < n && nums[i] > nums[left]) {
+//                        return false;
+//                    }
+//
+//                    if (right < n && nums[i] > nums[right]) {
+//                        return false;
+//                    }
+//                }
+//
+//                return true;
+//            }
+//        }
 
-            public boolean isHeap(int[] nums) {
+    public int[] minToMaxHeap(int[] nums) {
+        int n = nums.length;
 
-                int n = nums.length;
+        for(int i = n/2-1;i >= 0;i--)
+        {
+            heapify(nums , i ,n);
+        }
+        return nums;
+    }
+    private void heapify(int[] nums , int i , int n)
+    {
+        int largest = i;
 
-                for (int i = 0; i < n / 2; i++) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
 
-                    int left = 2 * i + 1;
-                    int right = 2 * i + 2;
-
-                    if (left < n && nums[i] > nums[left]) {
-                        return false;
-                    }
-
-                    if (right < n && nums[i] > nums[right]) {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
+        if(left < n && nums[left] > nums[largest])
+        {
+            largest = left;
+        }
+        if( right < n && nums[right] > nums[largest])
+        {
+            largest = right;
         }
 
-        public static void main(String[] args) {
+        if(largest!=i)
+        {
+            int temp = nums[i];
+            nums[i] = nums[largest];
+            nums[largest] = temp;
+            heapify(nums , largest , n);
+        }
 
-            int arr[] = {10, 20, 30, 25, 15};
+    }
+}
+
+
+public static void main(String[] args) {
+
+            int arr[] = {2 ,4 ,3 ,1};
 
             Solution s = new Solution();
 
-            boolean ans = s.isHeap(arr);
+            int[] ans = s.minToMaxHeap(arr);
 
-            System.out.print(ans);
+            for(int i = 0;i<ans.length;i++)
+            {
+                System.out.print(ans[i] + " ");
+            }
+
         }
     }
 
