a383d3edbea0eafd212fcdbac3b1b5841a1e87a8
shivapathak-code <shivapathak766@gmail.com>
2026-09-21 15:47:28 +0530

    fabonacci series

--- src/DSAAtoZproblems.java
+++ src/DSAAtoZproblems.java
@@ -533,6 +533,23 @@ public class DSAAtoZproblems {
 //            System.out.println("Not a magic Number");
 //        }
 //    }
+
+    public static void main(String[] args)  // Fibonacci Series
+    {
+        int n = 6;
+        int a = 0;
+        int b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        for(int i = 1;i<n;i++)
+        {
+            int c = a+b;
+            a = b;
+            b = c;
+
+            System.out.println(c);
+        }
+    }
 }
 
 
