748cfa7587db230bca7fee77f325f5da3f54c97a
shivapathak-code <shivapathak766@gmail.com>
2026-09-24 10:03:21 +0530

    print numbers 1 to 10

--- src/Recursion.java
+++ src/Recursion.java
@@ -2,18 +2,46 @@ public class Recursion {
     public static void main(String[] args)
     {
 
-        printHello(6);
+        //printHello(6);
+        //print1T010(10);
+        print10T01(10);
 
     }
-    public static void printHello(int n){
+//    public static void printHello(int n){
+//
+//        if(n == 0)
+//        {
+//            return;
+//        }
+//
+//        System.out.println("hello");
+//        printHello(n -1);
+//
+//    }
+
+//    public static void print1T010(int n){
+//
+//        if(n == 0)
+//        {
+//            return ;
+//        }
+//
+//
+//        print1T010(n -1);
+//        System.out.println(n);
+//
+//    }
+
+    public static void print10T01(int n){
 
         if(n == 0)
         {
-            return;
+            return ;
         }
 
-        System.out.println("hello");
-        printHello(n -1);
+        System.out.println(n);
+        print10T01(n -1);
+
 
     }
 }
